83f022f2c9a2ecf8941bb797a8c5696eba2155f8
Dhruv Gola <dhruvgola1203@gmail.com>
2026-06-14 08:35:34 +0530

    Resolve my orders and ispaid issue

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -4,9 +4,8 @@ import User from '../models/user.model.js';
 import stripe from 'stripe';
 export const placeOrder = async (req, res) => {
     try {
-        // console.log("USER ID FROM MIDDLEWARE:", req.body.userId);
-        // const userId = req.body.userId
-        const { userId, items, address } = req.body;
+        const userId = req.userId;
+        const { items, address } = req.body;
         if (!address || items.length === 0) {
             return res.json({ success: false, message: "invalid data" })
         }
@@ -30,7 +29,7 @@ export const placeOrder = async (req, res) => {
 }
 export const placeOrderStripe = async (req, res) => {
     try {
-        const userId = req.body.userId
+        const userId = req.userId;
         const { origin } = req.headers;
         const { items, address } = req.body;
         if (!address || items.length === 0) {
@@ -85,11 +84,9 @@ export const placeOrderStripe = async (req, res) => {
 }
 export const getUserOrders = async (req, res) => {
     try {
-        console.log("USER FROM AUTH MIDDLEWARE:", req.userId);
-        const { userId } = req.userId;
-        const orders = await Order.find({})
+        const userId = req.userId;
+        const orders = await Order.find({ userId })
             .populate("items.product").populate("address").sort({ createdAt: -1 })
-        console.log("ALL ORDERS:", orders);
         res.json({ success: true, orders })
     } catch (error) {
         res.json({ success: false, message: error.message })
@@ -102,7 +99,7 @@ export const getAllOrders = async (req, res) => {
                 { paymentType: "COD" },
                 { isPaid: true }
             ]
-        }).populate("items.product address")
+        }).populate("items.product address").sort({ createdAt: -1 })
         res.json({ success: true, orders })
     } catch (error) {
         res.json({ success: false, message: error.message })
@@ -110,7 +107,7 @@ export const getAllOrders = async (req, res) => {
 }
 export const stripeWebhooks = async (req, res) => {
     const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
-    const sig = request.headers["stripe-signature"];
+    const sig = req.headers["stripe-signature"];
     let event;
     try {
         event = stripeInstance.webhooks.constructEvent(
@@ -119,33 +116,19 @@ export const stripeWebhooks = async (req, res) => {
             process.env.STRIPE_WEBHOOK_SECRET
         );
     } catch (error) {
-        res.status(400).send(`Webhooks Error:${error.message}`)
+        return res.status(400).send(`Webhooks Error:${error.message}`)
     }
     switch (event.type) {
-        case "payment_intent.succeeded": {
-            const paymentIntent = event.data.object;
-            const paymentIntentId = paymentIntent.Id;
-            const session = await stripeInstance.checkout.sessions.list({
-                payment_intent: paymentIntentId
-            });
-            const { orderId, userId } = session.data[0].metadata;
-            await Order.findByIdAndUpdate(orderId,{isPaid:true})
-            await User.findByIdAndUpdate(userId,{cartItems: {}})
+        case "checkout.session.completed": {
+            const session = event.data.object;
+            const { orderId, userId } = session.metadata;
+            await Order.findByIdAndUpdate(orderId, { isPaid: true })
+            await User.findByIdAndUpdate(userId, { cartItems: {} })
             break;
         }
-         case "payment_intent.payment_failed":{
-            const paymentIntent = event.data.object;
-            const paymentIntentId = paymentIntent.Id;
-            const session = await stripeInstance.checkout.sessions.list({
-                payment_intent: paymentIntentId
-            });
-            const { orderId } = session.data[0].metadata;
-            await Order.findByIdAndUpdate(orderId);
-            break;
-         }
         default:
             console.log(`Unhandled event type ${event.type}`)
             break;
     }
-    res.json({received:true})
+    res.json({ received: true })
 }
